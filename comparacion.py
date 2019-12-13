e3cb8ccbb5ee195ad992ce8c5f666473dffb008e
EduChancafe <57601238+educhancafe@users.noreply.github.com>
2019-12-13 14:31:53 -0500

    se crearon los 15 ejercicios de comparacion

---
+++ comparacion.py
@@ -0,0 +1,90 @@
+#ejercicio1
+str="Buenas noches, edu"
+tex="Buenas noches"
+print(str==tex)
+
+
+#ejercicio2
+str=" te amo"
+res="yo tambien"
+if(str != res):
+    print("yo mucho mas")
+
+
+#ejercicio3
+parcial="19"
+if(parcial<="19" or nota=="19"):
+    print("aprovaste")
+
+
+#ejercicio4
+str1="Estoy solo"
+str2="Estoy acompañado"
+print(str1==str2)
+
+
+#ejercicio5
+str="Que tal Eduardo"
+str2="que tal eduardo"
+print(str>str2)
+
+
+#ejercicio6
+str="Soy el mejor"
+str2="cansado"
+print(str>str2)
+
+
+#ejercicio7
+str="Te amo Peru"
+str2="unida la costa,unida la sierra y unida la selva"
+print(str<str2)
+
+
+#ejercicio8
+str="casado"
+str2="soltero"
+print(str>str2)
+
+
+#ejercicio9
+str="familia"
+str2="Universitario"
+print(str>str2)
+
+
+#ejercicio10
+str="Me voy a la fiesta"
+str2="hoy me quedo a estudiar"
+print(str>str2)
+
+
+#ejercicio11
+msg=input("==<:")
+str="amor"
+if(str==msg):
+    print("hoy salimos")
+
+
+#ejercicio12
+str="voy a aprovar el curso"
+str2="voy a desaprovar el curso"
+print(str>str2)
+
+
+#ejercicio13
+str="tengo sueño"
+str2="tengo hambre"
+print(str>str2)
+
+
+#ejercicio14
+msg="el profesor enseña bien"
+msg2="el profesor enseña muy bien"
+print(str>str2)
+
+
+#ejercicio15
+str="te quiero como amigo"
+str2="yo tambien"
+print(str>str2)
