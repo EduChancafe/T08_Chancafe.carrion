311cc6dddbc568138035d9d5e7691f409f98acc6
EduChancafe <57601238+educhancafe@users.noreply.github.com>
2019-12-13 14:40:04 -0500

    se crearon los 15 ejercicios de operaciones

---
+++ operacion.py
@@ -0,0 +1,86 @@
+#ejercicio1
+text=input("ingrese apellido:")
+print(text.upper())               #retorna "Chancafe"
+
+
+#ejercicio2
+ruc="143526178"
+print(ruc.isdigit())    #retorna :False
+
+
+#ejercicio3
+oye="      te quiero   "
+print(oye.strip().upper())      #retorna:Te quiero
+
+
+#ejercicio4
+text="Sos la mas bella"
+print(text.upper().split(""))
+#retora:¨Sos¨,la,mas,bella
+
+
+#ejercicio5
+msg="Se que lo lograras"
+print(msg.lower().upper())
+#retorna:SE QUE LO LOGRARAS
+
+
+#ejercicio6
+cta="CIENCIA,TECNOLOGIA Y AMBIENTE"
+print(per.lower().split("."))
+#retorna:¨Ciencia, Tecnologia y Ambiente¨
+
+
+
+
+#ejercicio7
+numero_de_celular="982945464"
+print(numero_de_celular.isdigit())  #retorna: False
+
+
+# ejemplo 07
+ter="mi amigo tiene 23 años"
+print(ter.isdigit())
+# retorna:Falso
+
+
+#ejercicio8
+mon="Estudio todos los dias"
+print(mon.isdigit())      #retorna:True
+
+
+#ejercicio9
+text="Hoy estamos miercoles"
+print(text.split("."))     #retorna:Hoy estamos miercoles
+
+
+
+#ejercicio10
+git="juntos/todo/se/puede"
+print(git.split("/"))      #retorna:´juntos´,´todo´,´se´,´puede´
+
+
+#ejercicio11
+son="Tenemos fe en ti"
+print(son.lstrip())     #retorna:Tenemos fe en ti
+
+
+
+#ejercicio12
+msg=" nunca te rindas"
+print(msg.strip())      #retorna:nunca te rindas
+
+
+#ejercicio13
+tex="Eres el mejor jugador de voley"
+print(tex.upper())      #retorna:Eres el mejor jugador de voley
+
+
+#ejercicio14
+msg="Dale tiempo al tiempo"
+print(msg.lower())      #retorna:dale tiempo al tiempo
+
+
+#ejercicio15
+msg="No tengo enamorada"
+print(msg.lower())      #retorna:No tengo enamorada
