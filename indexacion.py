8122f70f1d647b9244493d7dc11ef3b64288e561
EduChancafe <57601238+educhancafe@users.noreply.github.com>
2019-12-13 14:29:23 -0500

    se crearon los 15 ejercicios de indexacion

---
+++ indexacion.py
@@ -0,0 +1,133 @@
+#ejercicio1
+
+#     0       10        20
+#    012345678901234567890
+
+str="EL AMOR ES MAS FUERTE"
+print(str[11],str[-9],str[13])          #mostrara la palabra "MAS"
+
+
+#ejercicio2
+
+#      0         10
+#      01234567890123
+
+str="MI NIÑA BONITA"
+print(str[8],str[-5],str[10],str[-3],str[12],str[-1])    #mostrara la palabra "BONITA"
+
+
+#ejercicio3
+
+#      0         10
+#     01234567890123456
+
+str="DEJALA QUE VUELVA"
+print(str[7],str[-5],str[1])                     #mostrara la palabra "QUE"
+
+
+#ejercicio4
+
+#       0       10
+#      012345678901234567
+
+str="ANDAS EN MI CABEZA"
+print(str[0],str[-17],str[2],str[-15],str[4])               #mostrara la palabra "ANDAS"
+
+#ejercicio5
+
+#        0        10
+#      012345678901234
+
+str="NO LLORES MUJER"
+print(str[10],str[-4],str[12],str[-2],str[14])         #mostrara la palabra "MUJER"
+
+
+#ejercicio6
+
+#      0         10
+#     0123456789012345678
+
+str="ME ESTOY ENAMORANDO"
+print(str[0],str[-16])                         #mostrara la palabra "ME"
+
+
+#ejercicio7
+
+#        0       10
+#      01234567890123456
+
+str="CUANDO ME ENAMORO"
+print(str[10],str[-6],str[12],str[-4],str[14],str[-2],str[16])      #mostrara la palabra "ENAMORO"
+
+
+
+#ejercicio8
+
+#         0       10
+#        0123456789012
+
+str="TE VOY A AMAR"
+print(str[3],str[-9],str[5])                          #mostrara la palabra "VOY"
+
+
+#ejercicio9
+
+#       0       10
+#     01234567890
+
+str="YO QUISIERA"
+print(str[3],str[-7],str[5],str[-5],str[7],str[-3],str[9],str[-1])     #mostrara la palabra "QUISIERA"
+
+
+#ejercicio10
+
+#      0        10
+#     01234567890123
+
+str="EL PRIMER BESO"
+print(str[-4],str[11],str[-2],str[13])   #mostrara la palabra "BESO"
+
+
+#ejercicio11
+
+#       0       10
+#      012345678901
+
+str="PARA TU AMOR"
+print(str[8],str[-3],str[10],str[-1])        #mostrara la palabra "AMOR"
+
+
+#ejercicio12
+
+#       0          10      20
+#      01234567890123456789012
+
+str="ESTOY VIVIENDO UN SUEÑO"
+print(str[18],str[-4],str[20],str[-2],str[22])     #mostrara la palabra "SUEÑO"
+
+
+#ejercicio13
+
+#        0        10
+#       012345678901
+
+str="UNA AVENTURA"
+print(str[4],str[-7],str[6],str[-5],str[8],str[-3],str[10],str[-1])      #mostrara la palbra "AVENTURA"
+
+
+#ejercicio14
+
+#      0        10
+#     012345678901
+
+str="NO HAY NADIE"
+print(str[7],str[-4],str[9],str[-2],str[11])        #mostrara la palabra "NADIE"
+
+
+#ejercicio15
+
+#      0         10
+#    012345678901
+
+str="LLORO POR TI"
+print(str[0],str[-11],str[2],str[-9],str[4],str[-7])     #mostrara la palabra "LLORO"
