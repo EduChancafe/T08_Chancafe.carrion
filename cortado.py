f89c53dcd8ba58366d63a38d61ddf6c5d6d7a26c
EduChancafe <57601238+educhancafe@users.noreply.github.com>
2019-12-13 14:35:36 -0500

    se crearon los 15 ejercicios de cortado

---
+++ cortado.py
@@ -0,0 +1,147 @@
+#ejercicio1
+
+#              10        20
+#     0123456789012345678901234
+
+msg="En la vida todo progresa"
+
+print(msg[6:10])              #se imprimira la cadena "vida"
+
+
+#ejercicio2
+
+#                 10       20         30        40
+#       012345678901234567890123456789012345678901234567
+
+msg="Como las estrellas, cada quien con su luz propia"
+
+print(msg[9:18]+" "+msg[37:41])      #se imprimira la cadena "Estrellas luz"
+
+
+#ejercicio3
+
+#                10         20        30
+#       0123456789012345678901234567890123456
+
+msg="Hay que mirar mas alla de lo que vez"
+
+print(msg[8:13]+" "+msg[14:18]+""+msg[18:23])     #impirmira la cadena "mirar mas alla"
+
+
+#ejercicio4
+
+#                  10       20
+#        012345678901234567890123
+
+msg="Donde hay amor, hay vida"
+
+print(msg[0:5]+" "+msg[5:8]+""+msg[18:24])
+
+
+#ejercicio5
+
+#               10        20
+#      012345678901234567890123456789
+
+msg="Hoy un lector, mañana un lider"
+
+print(msg[0:3]+""+msg[3:6]+" "+msg[25:39])
+
+#ejercicio6
+
+#              10        20        30
+#     0123456789012345678901234567890123456
+
+msg="Las oportunidades no pasan, las creas"
+
+print(msg[4:17])
+
+
+#ejercicio7
+
+#              10        20        30        40
+#      123456789012345678901234567890123456789012345
+
+msg="Un viaje muy largo se inicia con un solo paso"
+
+print(msg[0:2]+""+msg[12:17]+""+msg[39:45])
+
+
+#ejercicio8
+
+#               10        20
+#      01234567890123456789012345678
+
+msg="Piensa,crea,sueña y atrevete"
+
+print(msg[0:6]+""+msg[17:19]+" "+msg[20:28])
+
+
+#ejercicio9
+
+#               10        20
+#      012345678901234567890
+
+msg="Nunca dejes de crecer"
+
+print(msg[0:6])
+
+
+#ejercicio10
+
+#              10        20        30
+#     01234567890123456789012345678901234
+
+msg="La esperanza,es un sueño despierto"
+
+print(msg[19:24]+""+msg[24:34])
+
+
+#ejercicio11
+
+#               10        20        30
+#      012345678901234567890123456789012
+
+msg="El camino al exito es la actitud"
+
+print(msg[0:2]+" "+msg[13:18]+" "+msg[19:21]+" "+msg[22:24]+" "+msg[25:32])
+
+
+#ejercicio12
+
+#            10        20
+#   01234567890123456789012345678
+
+msg="Que todo fluya y nada influya"
+
+print(msg[0:4]+""+msg[4:8]+""+msg[20:29])
+
+
+#ejercicio13
+
+#               10        20        30
+#       0123456789012345678901234567890123456
+
+msg="Cada partido se juega como una final"
+
+print(msg[0:4]+" "+msg[5:12])
+
+
+#ejercicio14
+
+#                10        20        30
+#        12345678901234567890123456789012
+
+msg="El fracaso solo existe cuando te rindes"
+
+print(msg[0:2]+" "+msg[3:10]+" "+msg[16:22])
+
+
+#ejercicio15
+
+#
+#     1234567890123456789012345678901234
+
+msg="Ningun jugador es tan bueno como todos juntos"
+
+print(msg[7:14]+" "+msg[22:27])
