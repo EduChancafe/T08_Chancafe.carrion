b45ec659f95b199b336eacb7199952a3ee398324
EduChancafe <57601238+educhancafe@users.noreply.github.com>
2019-12-13 14:36:51 -0500

    se crearon los 15 ejercicios de iteracion

---
+++ iteracion.py
@@ -0,0 +1,118 @@
+#ejercicio1
+
+msg1="Eduardo"
+
+for text in msg1:
+    print(text)
+
+
+#ejercicio2
+
+frase="Solo se que nada se"
+
+for text in frase:
+    print(text)
+
+
+#ejercicio3
+
+carrera="Ingenieria Electronica"
+
+for text in carrera:
+    print(text)
+
+
+#ejercicio4
+
+deporte="Futbol"
+
+for text in deporte:
+     print(text)
+
+
+#ejercicio5
+
+apellido="Chancafe"
+
+for text in apellido:
+    print(text)
+
+
+#ejercicio6
+
+año="2018"
+
+for int in año:
+    print(int)
+
+
+#ejercicio7
+
+universidad="Pedro Ruiz Gallo"
+
+for university in universidad:
+    print(university)
+
+
+#ejercicio8
+
+nombre="xperia"
+
+for letra in nombre:
+    print(letra)
+
+
+#ejercicio9
+
+color="Azul"
+
+for blue in color:
+    print(blue)
+
+
+#ejercicio10
+
+curso="Fisica"
+
+for letra in curso:
+    print(letra)
+
+
+#ejercicico11
+
+estacion="Verano"
+
+for letra in estacion:
+    print(letra)
+
+
+#ejercicio12
+
+ciudad="Chiclayo"
+
+for letra in ciudad:
+    print(letra)
+
+
+#ejercicio13
+
+cuento="Caperusita Roja"
+
+for letra in cuento:
+    print(letra)
+
+
+#ejercicio14
+
+pokemon="picachu"
+
+for letra in pokemon:
+    print(letra)
+
+
+#ejercicio15
+
+cancion="Nuestro Sueño"
+
+for letra in cancion:
+    print(letra)
