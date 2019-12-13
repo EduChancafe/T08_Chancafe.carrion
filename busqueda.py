42d047d24b70e82f18210cce3b56c3db9e6840ff
EduChancafe <57601238+educhancafe@users.noreply.github.com>
2019-12-13 14:38:20 -0500

    se crearon los 15 ejercicios de busqueda

---
+++ busqueda.py
@@ -0,0 +1,148 @@
+#ejercicio1
+
+#       0        10        20
+#       1234567890123456789012
+cad="El señor de los anillos"
+
+print(cad.find("señor"))
+print(cad.index("señor"))
+
+
+#ejercicio2
+
+#     0        10
+#     0123456789012345678
+cad="Los enanitos verdes"
+
+print(cad.find("enanitos"))
+print(cad.index("verdes"))
+
+
+#ejercicio3
+
+#      0        10        20
+#      012345678901234567890123456
+cad="Una aventura extraordinaria"
+
+print(cad.find("aventura"))
+print(cad.index("extraordinaria"))
+
+
+#ejercicio4
+
+#      0        10
+#      012345678901234
+cad="Y se llama Peru"
+
+print(cad.find("llama"))
+print(cad.index("Peru"))
+
+
+#ejercicio5
+
+#      0        10
+#      012345678901234567
+cad="La nave del olvido"
+
+print(cad.find("nave"))
+print(cad.index("olvido"))
+
+
+#ejercicio6
+
+#       0        10        20
+#       01234567890123456789012
+cad="Contigo en la distancia"
+
+print(cad.find("Contigo"))
+print(cad.index("distancia"))
+
+
+#ejercicio7
+
+#       0        10        20
+#       012345678901234567890
+cad="Por debajo de la mesa"
+
+print(cad.find("Por"))
+print(cad.index("l"))
+
+
+#ejercicio8
+
+#     0        10        20
+#     012345678901234567890
+cad="Los aretes de la vida"
+
+print(cad.find("aretes"))
+print(cad.index("vida"))
+
+
+#ejercicio9
+
+#     0        10
+#     0123456789012345678
+cad="Viva el Peru"
+
+print(cad.find("Viva"))
+print(cad.index("u"))
+
+
+#ejercicio10
+
+#       0        10
+#       01234567890123456789
+cad="Yo no naci para amar"
+
+print(cad.find("Yo"))
+print(cad.index("r"))
+
+
+#ejercicio11
+
+#       0        10        20
+#       012345678901234567890123
+cad="Voy a ser un gran profesional"
+
+print(cad.find("un"))
+print(cad.index("gran"))
+
+
+#ejercicio12
+
+#   0        10        20
+#   0123456789012345678901234567
+cad="Cuando quieras donde quieras"
+
+print(cad.find("o"))
+print(cad.index("donde"))
+
+
+#ejercicios13
+
+#      0        10        20
+#      012345678901234567890123
+cad="Estoy viviendo un sueño"
+
+print(cad.find("un"))
+print(cad.index("Estoy"))
+
+
+#ejercicio14
+
+#       0        10
+#       0123456789012345678
+cad="El Peru es hermoso"
+
+print(cad.find("El"))
+print(cad.index("es"))
+
+
+#ejercicio15
+
+#   0        10        20
+#   012345678901234567890
+cad="Nunca pares de soñar"
+
+print(cad.find("de"))
+print(cad.index("pares"))
