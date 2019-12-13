e76d7c8496bdfcc09a884abece75b6b86ec59c2c
EduChancafe <57601238+educhancafe@users.noreply.github.com>
2019-12-13 14:30:35 -0500

    se crearon los 15 ejercicios de longitud

---
+++ longitud.py
@@ -0,0 +1,88 @@
+#ejercicio1
+nombre="eduardo"
+longitud=len(nombre)
+print("la longitud de la cadena es :",longitud)
+
+
+#ejercicio2
+apellido="chancafe"
+longitud=len(apellido)
+print("la longitud de la cadena es:",longitud)
+
+
+#ejercicio3
+deporte="futbol"
+longitud=len(deporte)
+print("la longitud de la cadena es:",longitud)
+
+
+#ejercicio4
+curso="programacion"
+longitud=len(curso)
+print("la longitud de la cadena es:",longitud)
+
+
+#ejercicio5
+enamorada=""
+longitud=len(enamorada)
+print("la longitud de la cadena es:",longitud)
+
+
+#ejercicio6
+cancion="desconocidos"
+longitud=len(cancion)
+print("la longitud de la cancion es:",longitud)
+
+
+#ejercicio7
+marca="  ADIDAS  "
+longitud=len(marca)
+print("la longitud de la cadena es:",longitud)
+
+
+#ejercicio8
+operacion="  iteracion"
+longitud=len(operacion)
+print("la longitud de la cadena es:",longitud)
+
+
+#ejercicio9
+hotel="´Casa Andina´"
+longitud=len(hotel)
+print("la longitud de la cadena es:",longitud)
+
+
+#ejercicio10
+deporte="voley/playa"
+longitud=len(deporte)
+print("la longitud de la cadena es:",longitud)
+
+
+#ejercicio11
+operador="claro   "
+longitud=len(operador)
+print("la longitud de la cadena es:",longitud)
+
+
+#ejercicio12
+cantante="´Bruno Mars´"
+longitud=len(cancion)
+print("la longitud de la cadena es:",longitud)
+
+
+#ejercicio13
+electrodomestico="lavadora   "
+longitud=len(electrodomestico)
+print("la longitud de la cadena es:",longitud)
+
+
+#ejercicio14
+codigo=" 195695D "
+longitud=len(codigo)
+print("la longitud de la cadena es:",longitud)
+
+
+#ejercicio15
+profesor="FRANK CHIRINOS"
+longitud=len(profesor)
+print("la longitud de la cadena es:",longitud)
