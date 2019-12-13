cc1a48eed1891ab746e72581a3ec98c1e5e3cd71
EduChancafe <57601238+educhancafe@users.noreply.github.com>
2019-12-13 14:33:43 -0500

    se crearon los 15 ejercicios de concatenacion

---
+++ concatenacion.py
@@ -0,0 +1,143 @@
+#operacion - concatenacion
+#    0         10        20        30        40
+#    01234567890123456789012345678901234567890123456789
+str="MIGUEL GRAU: EL CABALLERO DE LOS MARES, ES PERUANO"
+
+#acceso via indice
+#print(str[0]+str[1]+str[2]+str[3]+str[4]+str[5])
+
+
+#ejercio 2
+alumno="JUAN CARLOS"
+curso="BASE DE DATOS"
+nota=18
+condicion="APROBADO"
+#print("El alumno", alumno,"llevo el curso",curso,"\n","y su nota es", nota, ",su condicion es", condicion)
+
+
+
+#ejercicio1
+estudiante="´EDUARDO CHANCAFE´"
+carrera="INGENIERIA ELECTRONICA"
+universidad="PEDRO RUIZ GALLO"
+
+print("El estudiante",estudiante,"ingreso a la carrera",carrera,"\n","de la universidad",universidad)
+
+
+
+#ejercicio2
+señor="Juan Carrilo"
+premio="10000 soles"
+juego="la ´TINKA´"
+
+print("El señor",señor,"gano el premio de",premio,"\n","del juego",juego)
+
+
+
+#ejercicio3
+equipo="BARCELONA FC"
+torneo=" UEFA CHAMPIONS LEAGUE"
+año="2016"
+entrenador="Josep Guardiola"
+
+print("El equipo",equipo,"gano el torneo",torneo,"del año",año,"\n","teniendo como entrenador a",entrenador)
+
+
+
+#ejercicio4
+fecha="28 de Julio"
+año="1821"
+comandante="Don Jose De san Martin"
+pais="PERU"
+print("La fecha del",fecha,"del año",año,"el comandante",comandante,"\n","proclamo la independencia del pais",pais)
+
+
+
+#ejercicio5
+nota="18"
+examen="PROGRAMACION"
+condicion="APROVATORIA"
+
+print("Tengo que sacar de nota",nota,"en el examen de",examen,"\n","para que mi condicion sea",condicion)
+
+
+
+#ejercicio6
+pais1="PERU"
+pais2="RUSIA"
+año=2018
+
+print("El pais de",pais1,"clasifico al mundial de",pais2,"en el año del",2018)
+
+
+#ejercicio7
+estudiante="Jose Miguel"
+curso="Matematica"
+ciudad="Arequipa"
+
+print("El estudiante",estudiante,"gano el cuncurso nacional del curso de",curso,"\n","realizado en la ciudad de",ciudad)
+
+
+#ejercicio8
+almirante="Miguel Grau"
+barco="Monitor Huascar"
+pais="Chile"
+
+print("El Almirante",almirante,"murio en su barco",barco,"\n","en una batalla contra el pais de",pais)
+
+
+#ejercicio9
+cancion="Despacito"
+cantante="Luis Fonsi"
+año="2017"
+
+print("La cancion",cancion,"compuesta por el cantante",cantante,"\n","y fue la mas escuchada del año",año)
+
+
+#ejercicio10
+computadora="ENIEC"
+inventor="KONRAD ZUSE"
+año="1941"
+
+print("La primera compuradora llamada",computadora,"fue diseñada por el inventor",inventor,"\n","en el año",año)
+
+
+#ejercicio11
+dias="Lunes"
+ciudad="Chiclayo"
+plato="Espesado"
+
+print("Los dias",dias,"es tipico en la ciuadad",ciudad,"\n","comer el plato tipico llamado",plato)
+
+
+#ejercicio12
+jugador="Jefferson Farfan"
+pelicula="´El 10 de la Calle´"
+dia="30 de Noviembre"
+
+print("El jugador de futbol",jugador,"estrenara su pelicula llamada","\n",pelicula,"el dia",dia)
+
+
+#ejercicio13
+alumno="Angel Castro"
+curso="Fisica"
+nota="15"
+condicion="Desaprovatoria"
+
+print("El alumno",alumno,"saco en el examen del curso",curso,"\n","nota",nota,",y su condicion es",condicion)
+
+
+#ejercicio14
+actor="Leonardo Dicaprio"
+premio="Oscar"
+categoria="Mejor actor de Reparto"
+
+print("El actor",actor,"gano el premio",premio,"\n","en la categoria",categoria)
+
+
+#ejercicio15
+pelicula="Avatar"
+año="2012"
+premio="oscar"
+
+print("La pelicula",pelicula,"gano en el año",año,"\n","un total de 8 premios",premio,"batiendo un record mundial")
